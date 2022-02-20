5b4c767ad5ad11c8f618756a6342896c3dc2446e
Jayesh Choudhary <choudharyjayesh333@gmail.com>
2022-02-20 12:18:10 +0530

    exposed driver in transaction

--- src/modules/db/transaction.ts
+++ src/modules/db/transaction.ts
@@ -6,35 +6,35 @@ import { Insert } from "./insert";
 import { Update } from "./update";
 
 export class DBTransaction {
-  private trx: Transaction;
+  driver: Transaction;
 
   constructor(trx: Transaction) {
-    this.trx = trx;
+    this.driver = trx;
   }
 
   get(tableName: string): Get {
-    return new Get(this.trx, tableName, OperationType.All);
+    return new Get(this.driver, tableName, OperationType.All);
   }
 
   getOne(tableName: string): Get {
-    return new Get(this.trx, tableName, OperationType.One);
+    return new Get(this.driver, tableName, OperationType.One);
   }
 
   insert(tableName: string): Insert {
-    return new Insert(this.trx, tableName);
+    return new Insert(this.driver, tableName);
   }
 
   update(tableName: string): Update {
-    return new Update(this.trx, tableName);
+    return new Update(this.driver, tableName);
   }
 
   delete(tableName: string): Delete {
-    return new Delete(this.trx, tableName);
+    return new Delete(this.driver, tableName);
   }
 
   async rawExec<T>(sql: string, params: JSONMap): Promise<T> {
     return new Promise((resolve, reject) => {
-      this.trx
+      this.driver
         .raw(sql, params)
         .then((res) => {
           resolve(res.rows);
